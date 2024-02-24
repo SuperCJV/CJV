47500e7c16ef9e57862867a5e3980d6dc75458ac
Cristian Victoria <62697402+SuperCJV@users.noreply.github.com>
2024-02-24 12:12:38 -0500

    Create viewjoinedleagues.php

---
+++ viewjoinedleagues.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>viewjoinedleagues.php</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+        <style>
+            body, h1, h2, h3, h4, h5 {font-family: "Poppins", sans-serif}
+            body {font-size:16px;}
+            .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
+            .w3-half img:hover{opacity:1}
+        </style>
+    </head>
+    <body>
+
+        <!-- Sidebar/menu -->
+        <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar">
+            <br>
+            <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
+            <div class="w3-container">
+                <h3 class="w3-padding-64"><b>Ultimate League</b></h3>
+                <div class='w3-bar-block'>
+                    <a href='/php_ib/ultimatefootball_database/homepage.php' onclick='w3_close()' class='w3-bar-item w3-button w3-hover-white'>Home</a>
+                    <a href='/php_ib/ultimatefootball_database/logout.php' onclick='w3_close()' class='w3-bar-item w3-button w3-hover-white'>Logout</a>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Top menu on small screens -->
+        <header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+            <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
+            <span>Ultimate League</span>
+        </header>
+
+        <!-- Overlay effect when opening sidebar on small screens -->
+        <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+        <!-- !PAGE CONTENT! -->
+        <div class="w3-main" style="margin-left:340px;margin-right:40px">
+
+            <!-- Header -->
+            <div class='w3-container' style='margin-top:80px' id='viewjoinedleagues'>
+                <h1 class='w3-jumbo'><b>View the leagues you have joined</b></h1>
+                <h1 class='w3-xxxlarge w3-text-red'><b>What would you like to do?</b></h1>
+                <hr style='width:50px;border:5px solid red' class='w3-round'>
+                <?php
+                include 'topper.php';
+                $servername = "localhost";
+                $username = "Cristian";
+                $password = "Thisiscristian";
+                $dbname = "UltimateFootball";
+
+                // Create connection
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $currentusernameid = $_SESSION["currentuser"];
+                $sql1 = "SELECT `userid` FROM `user` WHERE `username` = '$currentusernameid'";
+                $result = mysqli_query($conn, $sql1);
+                if ($row = mysqli_fetch_assoc($result)) {
+                    $useriduserid = $row['userid'];
+
+                    $leagueids = array();
+
+                    $sql2 = "SELECT leagueid from players WHERE userid = '$useriduserid'";
+                    $result = mysqli_query($conn, $sql2);
+                    if ($result->num_rows > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $leagueids[] = $row['leagueid'];
+                        }
+                    }
+
+                    echo "<br><table><tr><th>League name</th><th>Insert predictions</th><th>View leaderboard</th></tr>";
+                    foreach ($leagueids as $leagueidd) {
+                        $sql3 = "SELECT `leaguename` FROM `league` WHERE `leagueid` = '$leagueidd'";
+                        $result = mysqli_query($conn, $sql3);
+                        if ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr><td>" . htmlspecialchars($row['leaguename']) . "</td>
+                            <td><a href='predictioninput.php?id=" . htmlspecialchars($leagueidd) . "'>Click</a></td>
+                            <td><a href='leaderboard.php?id=" . htmlspecialchars($leagueidd) . "'>Click</a></td></tr>";
+                        }
+                    }
+                    echo "</table>";
+                } else {
+                    echo "You have not joined any leagues.";
+                }
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
